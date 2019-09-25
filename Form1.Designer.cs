163e82b2f26e3647e086e77b289cfc4ce8cd419e
minhduc276 <minhduc2761998@gmail.com>
2019-09-25 22:02:41 +0700

    Up slideshow anh
    Signed-off-by: minhduc276 <minhduc2761998@gmail.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace slide_show
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.picShow = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.linkChooseImage = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picShow)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.picShow);
+            this.panel1.Location = new System.Drawing.Point(56, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(687, 285);
+            this.panel1.TabIndex = 0;
+            // 
+            // picShow
+            // 
+            this.picShow.Location = new System.Drawing.Point(0, 0);
+            this.picShow.Name = "picShow";
+            this.picShow.Size = new System.Drawing.Size(687, 285);
+            this.picShow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picShow.TabIndex = 0;
+            this.picShow.TabStop = false;
+            this.picShow.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.pic5);
+            this.panel2.Controls.Add(this.pic4);
+            this.panel2.Controls.Add(this.pic3);
+            this.panel2.Controls.Add(this.pic2);
+            this.panel2.Controls.Add(this.pic1);
+            this.panel2.Location = new System.Drawing.Point(56, 338);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(687, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // pic5
+            // 
+            this.pic5.Location = new System.Drawing.Point(550, 0);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(137, 100);
+            this.pic5.TabIndex = 4;
+            this.pic5.TabStop = false;
+            // 
+            // pic4
+            // 
+            this.pic4.Location = new System.Drawing.Point(401, 0);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(143, 100);
+            this.pic4.TabIndex = 3;
+            this.pic4.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(259, 0);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(136, 100);
+            this.pic3.TabIndex = 2;
+            this.pic3.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(127, 0);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(126, 100);
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(0, 0);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(121, 100);
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // linkChooseImage
+            // 
+            this.linkChooseImage.AutoSize = true;
+            this.linkChooseImage.Location = new System.Drawing.Point(746, 314);
+            this.linkChooseImage.Name = "linkChooseImage";
+            this.linkChooseImage.Size = new System.Drawing.Size(53, 13);
+            this.linkChooseImage.TabIndex = 5;
+            this.linkChooseImage.TabStop = true;
+            this.linkChooseImage.Text = "Chon anh";
+            this.linkChooseImage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkChooseImage_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkChooseImage);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picShow)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picShow;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.LinkLabel linkChooseImage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slide_show
+{
+    public partial class Form1 : Form
+    {
+        Image image;
+        Image[] arrImage;
+        string[] files;  //mảng chưa các files trong folder
+        int imgShow = 0;    //số vị trí của ảnh trong mảng arrImage
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (imgShow <= arrImage.Length - 1)
+            {
+                changeImage(imgShow);
+                imgShow++;
+            }
+            else
+            {
+                imgShow = 0;
+                changeImage(imgShow);
+            }
+        }
+
+        private void changeImage(int imgChoosed)
+        {
+            //change main image
+            picShow.Image = arrImage[imgChoosed];
+
+            //change list image
+            if (imgChoosed >= arrImage.Length)
+                imgChoosed = 0;
+            pic1.Image = arrImage[imgChoosed++];
+            if (imgChoosed >= arrImage.Length)
+                imgChoosed = 0;
+            pic2.Image = arrImage[imgChoosed++];
+            if (imgChoosed >= arrImage.Length)
+                imgChoosed = 0;
+            pic3.Image = arrImage[imgChoosed++];
+            if (imgChoosed >= arrImage.Length)
+                imgChoosed = 0;
+            pic4.Image = arrImage[imgChoosed++];
+            if (imgChoosed >= arrImage.Length)
+                imgChoosed = 0;
+            pic5.Image = arrImage[imgChoosed++];
+        }
+
+        private void linkChooseImage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FolderBrowserDialog folderdialog = new FolderBrowserDialog();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Chọn ảnh đại diện";
+
+            if (folderdialog.ShowDialog() == DialogResult.OK)
+            {
+                files = System.IO.Directory.GetFiles(folderdialog.SelectedPath, "*.jpg", System.IO.SearchOption.TopDirectoryOnly);
+            }
+
+            if (files.Length > 0)
+            {
+                timer1.Enabled = true;
+                arrImage = new Image[files.Length];
+                for (int i = 0; i < files.Length; i++)
+                {
+                    image = Image.FromFile(files[i]);
+                    arrImage[i] = image;
+                }
+            }
+            else
+            {
+                MessageBox.Show("File ảnh không hợp lệ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }       
+        }
+    }
+}
